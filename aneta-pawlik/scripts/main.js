12277fdcdcbfa4a0d5571c5164894492dfad53cd
AKP23uni <152009707+AKP23uni@users.noreply.github.com>
2023-12-20 23:26:09 +0100

    Add files via upload

---
+++ aneta-pawlik/scripts/main.js
@@ -0,0 +1,15 @@
+var btn = $('#backtotop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
